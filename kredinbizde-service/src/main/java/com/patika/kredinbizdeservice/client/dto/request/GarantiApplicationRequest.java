cec25575dddc1e1cf5a2b02ceca5335a7facaa0f
Batuhan Akbaş <batuhanakbas011@gmail.com>
2024-03-30 20:10:52 +0300

    Add Garanti bank client

---
+++ kredinbizde-service/src/main/java/com/patika/kredinbizdeservice/client/dto/request/GarantiApplicationRequest.java
@@ -0,0 +1,17 @@
+package com.patika.kredinbizdeservice.client.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class GarantiApplicationRequest {
+
+    private Long userId;
+}
